d6dcb3dbc81fbf8ed98cc02b19d8346717e4e5dd
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2021-02-07 20:32:24 +0700

    feat: filter unused data

--- src/components/LiveCases/index.js
+++ src/components/LiveCases/index.js
@@ -57,7 +57,7 @@ class LiveCases extends HTMLElement {
   _renderIndonesiaLiveCases() {
     LiveCases.fetchIndonesiaCovid19Stats().then(indonesiaCovid19Stats => {
       indonesiaCovid19Stats
-        .data
+        .data.slice(0, 34)
         .sort((a, b) => b.kasusPosi - a.kasusPosi)
         .forEach(provinceCovid19Stats => {
           const provinceName = provinceCovid19Stats.provinsi;
